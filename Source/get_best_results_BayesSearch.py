1ee58b53c3f84524ee7a197da51cea6a39281552
bilkentli88 <168015522+bilkentli88@users.noreply.github.com>
2024-05-09 20:49:34 +0300

    Add files via upload

---
+++ Source/get_best_results_BayesSearch.py
@@ -0,0 +1,18 @@
+
+import sys
+import helper_experiments
+import dataset_names
+
+if __name__ == "__main__":
+	#print(sys.argv)
+	liste = dataset_names.get_database_list_from_arguments(sys.argv)
+	print(liste)
+	for dataset_name in liste:
+		helper_experiments.get_best_result_for_one_dataset(
+			search_type="BayesSearch"
+			,dataset_name= dataset_name
+			,n_iter=100
+			,search_max_epoch = 100
+			,best_result_max_epoch= 5000
+			)
+
