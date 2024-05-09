dafd354be7e606b71da8a9fa416d2e68f4b0061c
bilkentli88 <168015522+bilkentli88@users.noreply.github.com>
2024-05-09 20:51:16 +0300

    Add files via upload

---
+++ Source/search_params_hyperopt_annealing.py
@@ -0,0 +1,17 @@
+import sys
+import os
+
+import dataset_names
+import helper_experiments
+
+if __name__ == "__main__":
+	liste = dataset_names.get_database_list_from_arguments(sys.argv)
+
+	print("datasets to work on")
+	print(liste)
+	search_type = "HyperoptSearchAnnealing"
+	for dataset_name in liste:
+		helper_experiments.find_result_for_one(
+			search_type, dataset_name, n_iter=100, search_max_epoch=100)
+	
+
---
+++ Source/search_params_optuna.py
@@ -0,0 +1,21 @@
+import sys
+import os
+
+from optuna import samplers
+
+import dataset_names
+import helper_experiments
+
+liste_samplers = ["Default", "NSGAIISampler", "CmaEsSampler", "MOTPESampler"]
+
+if __name__ == "__main__":
+	liste = dataset_names.get_database_list_from_arguments(sys.argv)
+
+	print("datasets to work on")
+	print(liste)
+	for dataset_name in liste:
+		for sampler in liste_samplers:
+			search_type = f"OptunaSearch{sampler}"
+			helper_experiments.find_result_for_one(search_type,dataset_name,100)
+	
+
