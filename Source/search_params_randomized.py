1e7f9b23f3b72dfe9e37d2e916f0ffeb78d5607b
bilkentli88 <168015522+bilkentli88@users.noreply.github.com>
2024-05-09 21:35:58 +0300

    Update search_params_randomized.py

--- Source/search_params_randomized.py
+++ Source/search_params_randomized.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 import dataset_names
-import helper_experiments
+import MainModule
 
 if __name__ == "__main__":
 	liste = dataset_names.get_database_list_from_arguments(sys.argv)
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 	print(liste)
 	search_type = "RandomizedSearch"
 	for dataset_name in liste:
-		helper_experiments.find_result_for_one(search_type,dataset_name,100)
+		MainModule.find_result_for_one(search_type,dataset_name,100)
 	
 
 	
