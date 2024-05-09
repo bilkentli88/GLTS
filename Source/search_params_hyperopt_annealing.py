842b4d082179e02f582a2d4ccbfbac285f382feb
bilkentli88 <168015522+bilkentli88@users.noreply.github.com>
2024-05-09 21:33:04 +0300

    Update search_params_hyperopt_annealing.py

--- Source/search_params_hyperopt_annealing.py
+++ Source/search_params_hyperopt_annealing.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 import dataset_names
-import helper_experiments
+import MainModule
 
 if __name__ == "__main__":
 	liste = dataset_names.get_database_list_from_arguments(sys.argv)
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 	print(liste)
 	search_type = "HyperoptSearchAnnealing"
 	for dataset_name in liste:
-		helper_experiments.find_result_for_one(
+		MainModule.find_result_for_one(
 			search_type, dataset_name, n_iter=100, search_max_epoch=100)
 	
 
